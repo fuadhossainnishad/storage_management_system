1160cea4749bbdbe7b6de785278a809f966bef52
fuadhossainnishad <fuadhossainbk01@gmail.com>
2025-03-27 00:37:22 +0600

    add helmet, express-rate-limit

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,22 @@ import dotenv from "dotenv";
 import { errorHandler } from "./middleware/errorHandler";
 import authRouter from "./user.Auth/auth.routes";
 import fileRouter from "./file/file.routes";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 const app = express();
 
 dotenv.config();
 app.use(express.json());
+app.use(helmet({
+    frameguard: { action: "deny" },
+    noSniff: true,
+}))
+
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later.",
+}))
 
 app.use("/user", authRouter);
 app.use("/storage", fileRouter);
